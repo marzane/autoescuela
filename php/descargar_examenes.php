2b1419bb48d2047a1bb09dc5a63776a112b597e2
marz <125163478+marzane@users.noreply.github.com>
2023-10-23 13:49:00 +0200

    Update descargar_examenes.php

--- php/descargar_examenes.php
+++ php/descargar_examenes.php
@@ -7,13 +7,14 @@
         
         $query_usuario_id = mysqli_query($conn, "select id from USUARIO where nombre=".$usuario.";");
         $usuario_id = mysqli_fetch_row(mysqli_result $query_usuario_id);
-        echo $usuario_id;
-        
+        //echo $usuario_id;
+
+        //if($usuario_id && $usuario_id[0])
     
         $query_examenes = mysqli_query($conn, "select e.id, e.fechahora, (select count(*) from RESULTADOPREGUNTA where resultado='aciertos' and examen=e.id) as aciertos,
                                                (select count(*) from RESULTADOPREGUNTA where resultado='fallos' and examen=e.id) as fallos
                                                from RESULTADOEXAMEN e join RESULTADOPREGUNTA p
-                                               on e.usuario=".$usuario." group by e.id order by e.id asc;");
+                                               on e.usuario=".$usuario_id[0]." group by e.id order by e.id asc;");
         
         $num_examenes = mysqli_num_rows($query_preguntas);
 
